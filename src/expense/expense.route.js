5214fce77013e24242a045dcafff66cfe29ef9b3
Adarsh <adarshkumarjha193@gmail.com>
2025-12-04 18:46:53 +0530

    Implement update and delete expense functionalities in controller and service

--- src/expense/expense.route.js
+++ src/expense/expense.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/addExpense', userController.checkLogin, expenseController.addExpense);
 router.get('/getExpenses', userController.checkLogin, expenseController.getExpenses);
 router.get('/monthly-expenses', userController.checkLogin, expenseController.getMonthlyExpenses);
-// router.put('/updateExpense/:id', expenseController.checkLogin, expenseController.updateExpense);
-// router.delete('/deleteExpense/:id', expenseController.checkLogin, expenseController.deleteExpense);
+router.put('/updateExpense/:id', userController.checkLogin, expenseController.updateExpense);
+router.delete('/deleteExpense/:id', userController.checkLogin, expenseController.deleteExpense);
 
 module.exports = router;
